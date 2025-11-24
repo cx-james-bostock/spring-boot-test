bdc1f9ad8963e4eca912bfb1d85364d00342ff03
James Bostock <77472194+cx-james-bostock@users.noreply.github.com>
2025-11-24 12:54:01 +1100

    Updates to get tests to pass

--- src/main/java/com/checkmarx/ps/restservice/CurrencyDetails.java
+++ src/main/java/com/checkmarx/ps/restservice/CurrencyDetails.java
@@ -1,13 +1,17 @@
 package com.checkmarx.ps.restservice;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CurrencyDetails {
+    @Id
     private Long id;
     private String currencyName;
 }
